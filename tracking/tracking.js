d2b746655e8f6c67140ddcfcfc891a41e2ebde75
Kavodel Ohiomoba <kohiomoba@kavodels-air-3.attlocal.net>
2014-07-31 09:59:07 -0700

    Initializing tracking file to send events

---
+++ tracking/tracking.js
@@ -0,0 +1,20 @@
+/* Event Tracking Library */
+
+var local = window.localStorage;
+
+function Tracking () {
+
+}
+
+
+Tracking.prototype.track = function() {
+
+
+}
+
+Tracking.prototype.queue = function() {
+
+
+}
+
+
