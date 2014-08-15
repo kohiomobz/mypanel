bccb8bf078eef038d1f3852ef22612cba6bd98e7
root <kohiomobz@gmail.com>
2014-08-15 15:40:41 -0400

    changed READ API

--- mypanel/mypanel/api/api.py
+++ mypanel/mypanel/api/api.py
@@ -3,78 +3,99 @@
 from django.db import connection
 from mypanel_app.models import Event
 import json
+from dateutil import parser
 
 def extract_params(request):
-		"""
-		Utility function to parse out query params from a django request
+        """
+        Utility function to parse out query params from a django request
 
-		"""
-		# Parse out the request parameters
-		request_params = request.GET if request.GET else request.POST
+        """
+        # Parse out the request parameters
+        request_params = request.GET if request.GET else request.POST
 
-		# Return dictionary with request params
-		request_dict = request_params.dict()
+        # Return dictionary with request params
+        request_dict = request_params.dict()
 
-		return request_dict
+        return request_dict
 
 
 class Import(object):
-	"""
-		Import Class with methods for insert and import Endpoints
-		Insert method deals with data that 
-		Both methods return either a 0 or a 1
-	"""
+    """
+        Import Class with methods for insert and import Endpoints
+        Insert method deals with data that 
+        Both methods return either a 0 or a 1
+    """
 
-	def __init__(self, req):
-		self.request = req
+    def __init__(self, req):
+        self.request = req
 
-	def insert(self):
-		request_dict = extract_params(self.request)
+    def insert(self):
+        request_dict = extract_params(self.request)
 
-		# Strip the double quotes and format as JSON  || If the data can't be formatted as JSON, return 0
-		try: 
-			dict_to_json = json.loads(request_dict['data'].replace("'", "\""))
-		except ValueError:
-			return 0
+        # Strip the double quotes and format as JSON  || If the data can't be formatted as JSON, return 0
+        try: 
+            dict_to_json = json.loads(request_dict['data'].replace("'", "\""))
+        except ValueError:
+            return 0
 
-		# Create an Event Object with the request dictionary
-		event_object = Event(name=dict_to_json.get('event'), time=dict_to_json.get('time').replace('T',' ').split('.')[0])
-		event_object.save()
+        # Create an Event Object with the request dictionary
+        event_object = Event(name=dict_to_json.get('event'), time=dict_to_json.get('time').replace('T',' ').split('.')[0])
+        event_object.save()
 
-		return 1
+        return 1
 
 
 class Query(object):
-	"""
-		Query Class returns SQL data for a client Query
-	"""
+    """
+        Query Class returns SQL data for a client Query
+    """
 
-	def __init__(self, req):
-		self.request = req
-	
-	
-	def extract_params(self,request):
-		"""
-		Utility function to parse out query params from a django request
+    def __init__(self, req):
+        self.request = req
 
-		"""
-		# Parse out the request parameters
-		request_params = request.GET if request.GET else request.POST
+    def extract_params(self,request):
+        """
+        Utility function to parse out query params from a django request
 
-		# Return dictionary with request params
-		request_dict = request_params.dict()
-
-		return request_dict
+        """
+        # Parse out the request parameters
+        request_params = request.GET if request.GET else request.POST
+        # Return dictionary with request params
+        print request_params
+        request_dict = request_params.dict()
+        return request_dict
 
 
-	def read(self):
-		# grab request parameters
-		request_dict = self.extract_params(self.request)
-		# format as JSON
-		if request_dict:
-		    dict_to_json = json.loads(request_dict['data'].replace("'", "\""))
-		## Now Query MySQL with date range, events, etc...
-		query = Event.objects.all().values()
-		print query
+    def read(self):
+        # grab request parameters
+        request_dict = self.extract_params(self.request)
+        from_date = request_dict.get('from_date')
+        to_date = request_dict.get('to_date')
+        query = None
 
-		return query
+        if from_date and to_date:
+            print from_date,to_date
+            newfrom = parser.parse(from_date)
+            newto = parser.parse(to_date)
+            print type(newfrom), type(newto)
+            query = Event.objects.filter(time__gt=newfrom).filter(time__lt=newto).values()
+
+        ## Now Query MySQL with date range, events, etc...
+
+        if not query: query  = Event.objects.all().values()
+
+        data = self.format_data(query)
+        return data
+
+    def format_data(self, event_list):
+        """
+            Take a list of events and return a formatted list where the values are summed by day
+        """
+        return event_list
+        """
+        events = []
+        event_dct = {} 
+        for val in event_list:
+            if event_dct.get(val['name']):
+                event_dct[val['name']]['date']
+        """
--- mypanel/mypanel/static/js/main.js
+++ mypanel/mypanel/static/js/main.js
@@ -1,59 +1,108 @@
-
-
 /* Request Data from the Dom */
 window.onload = function() {
-	
-	/* Build the Request URL */
-	function buildRequestURL(){
-	    /* var dateRange = getDateRange(); */
-	    	
-	    return '/query';	
-	}
-	
-	/* Fire the XHR Request to mypanel servers */
-	var makeDataRequest = function (){
-		var req = new XMLHttpRequest();
+    /* Submit a Date Query */
+    var fromInput = document.getElementsByName('from')[0];
+    var toInput = document.getElementsByName('to')[0];
+    var submit = document.getElementById('date-submit');
 
-		var url = buildRequestURL(); 
+    submit.onclick = function() {
+        event.preventDefault();
+        validDateQuery();
 
-		req.open('GET', url, true);
-		req.onload = function() {
-		    var data = JSON.parse(this.responseText);
-		    drawGraph(data, '');
-		    fillTable(data, '');
-		}	
-		req.send();
-	
-	}
-	
-	/* Highcharts Graphing Function */
-        var drawGraph = function(datums, dateRange){
-	
-	var event_list = [];
-	for (var i=0; i < datums.length; i++){
-	    if (datums[i]['name'] in event_list === false){
-		event_list.push(datums[i]['name']);
-	    }
-	}
-	
-	var seriesList = [];
-	
-	for (var i=0; i < datums.length; i++){
-	    var graphObject = {};
-	    graphObject['name'] = datums[i]['name'];
+    }
 
-	    /* Sum over the events in each bucket */
-	    graphObject['data'] = [0,1,2,3,4,5,6,7,8,9,10]
-	    seriesList.push(graphObject);
+    /* Populate the Event Dropdown */
+    var populateEvents = function(list) {
+        var select = document.getElementsByClassName('events')[0];
+        /* Add All to dropdown */
 
-	}	
+        var option = document.createElement('option');
+        option.text = 'All';
+        select.add(option);
 
-	$('#container').highcharts({
+        for (var i=0; i< list.length; i++){
+
+            var option = document.createElement('option');
+            option.text = list[i];
+            select.add(option);
+
+
+        }
+    }
+
+    function validDateQuery() {
+        if (!fromInput.value || !toInput.value) {
+            alert('please enter a valid date range');
+            return false;
+        }
+        makeDataRequest();
+    }
+
+    /* Build the Request URL */
+
+    function buildRequestURL(){
+        /* var dateRange = getDateRange(); */
+
+        var queryString = '/query?';
+        var fromDate = document.getElementsByName('from')[0].value;
+        var toDate = document.getElementsByName('to')[0].value;
+
+        if (fromDate && toDate ) {
+            queryString += 'from_date=' + fromDate + '&to_date=' + toDate;
+        }
+
+        return queryString;	
+    }
+
+    /* Fire the XHR Request to mypanel servers */
+    var makeDataRequest = function (){
+        var req = new XMLHttpRequest();
+
+        var url = buildRequestURL(); 
+
+        req.open('GET', url, true);
+        req.onload = function() {
+            console.log(this.responseText);
+            var data = JSON.parse(this.responseText);
+            drawGraph(data, '');
+            fillTable(data, '');
+        }
+        req.send();
+
+    }
+
+    /* Highcharts Graphing Function */
+    var drawGraph = function(datums, dateRange){
+
+        var event_list = [];
+        for (var i=0; i < datums.length; i++){
+            if (datums[i]['name'] in event_list === false){
+                event_list.push(datums[i]['name']);
+            }
+        }
+        populateEvents(event_list);
+        var seriesList = [];
+
+        var populateSeriesList = function(slist) {
+
+            for (var i=0; i < datums.length; i++){
+                var graphObject = {};
+                graphObject['name'] = datums[i]['name'];
+
+                /* Sum over the events in each bucket */
+                graphObject['data'] = [0,1,2,3,4,5,6,7,8,9,10]
+                seriesList.push(graphObject);
+
+            }
+        }
+        populateSeriesList(seriesList);
+
+        $('#container').highcharts({
             title: {
                 text: 'Mypanel Trends',
                 x: -20 //center
             },
-            
+
             xAxis: {
                 categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
                     'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -78,32 +127,30 @@ window.onload = function() {
                 borderWidth: 1
             },
             series: seriesList
-        	});
+        });
 
-	
-	    /* Remove Highcharts plug from the graph */
-	
-	    $('svg text:contains(Highcharts.com)').hide();
-	
-	    /* Remove the print menu in the top right corner of the graph */
-	
-	    $('g.highcharts-button').hide(); /* remove the export module instead */
-	}		
-	function fillTable(tableData, dateRange) {
-	    for (var i=0; i < tableData.length; i++) {
-		var table = document.getElementsByClassName('data-table')[0];
-		console.log(table);
-		var element = document.createElement('tr');
-		var html = '<td>' + tableData[i]['event_id'] + '</td><td>' + tableData[i]['name'] + '</td><td>' + tableData[i]['time'] + '</td>';
+        /* Remove Highcharts plug from the graph */
 
-		element.innerHTML = html;
-		console.log(element);
-		table.appendChild(element);
-	    }
+        $('svg text:contains(Highcharts.com)').hide();
 
-	}	
+        /* Remove the print menu in the top right corner of the graph */
 
-	/* Make the Request and Plot the Graph  */
-	
-	makeDataRequest();
+        $('g.highcharts-button').hide(); /* remove the export module instead */
+    }
+    
+        function fillTable(tableData, dateRange) {
+            for (var i=0; i < tableData.length; i++) {
+                var table = document.getElementsByClassName('data-table')[0];
+                var element = document.createElement('tr');
+                var html = '<td>' + tableData[i]['event_id'] + '</td><td>' + tableData[i]['name'] + '</td><td>' + tableData[i]['time'] + '</td>';
+
+                element.innerHTML = html;
+                table.appendChild(element);
+            }
+
+        }
+
+    /* Make the Request and Plot the Graph  */
+
+    makeDataRequest();
 }
